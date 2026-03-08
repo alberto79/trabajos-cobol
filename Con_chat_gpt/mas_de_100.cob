7f87cd6a6b5df80280abb049ae553c0fb986062f
alberto79 <albertoorta79@gmail.com>
2026-03-08 19:44:52 +0100

    initial commit

---
+++ Con_chat_gpt/mas_de_100.cob
@@ -0,0 +1,13 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAM-NAME.
+           
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+           
+       PROCEDURE DIVISION.
+       000-MAIN.
+            DISPLAY "Hello, World mas de 100!".          
+            GOBACK.
+        
+       
+
